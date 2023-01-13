35136f2c831957932791151317d9019f474f8e3e
Eric Greenhalgh <egreenhalgh@conservice.com>
2023-01-13 08:26:38 -0700

    Added social media icons to and styled footer.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,8 +3,8 @@ import React from "react"
 const Button = ({ text, color, bgColor, icon }) => {
   return (
     <button className="btn" style={{ color: color, backgroundColor: bgColor }}>
-      <span className="btn-icon">{icon}</span>
-      <span className="btn-text">{text}</span>
+      {icon ? <span className="btn-icon">{icon}</span> : null}
+      {text ? <span className="btn-text">{text}</span> : null}
     </button>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,27 @@
 import React from "react"
+import { Button } from "./"
+import { FaTwitterSquare } from "react-icons/fa"
+import { FaFacebookSquare } from "react-icons/fa"
+import { FaInstagramSquare } from "react-icons/fa"
+import { FaGithubSquare } from "react-icons/fa"
 
 const Footer = () => {
-  return <div>Footer</div>
+  return (
+    <footer className="footer">
+      <span className="social-icon">
+        <FaTwitterSquare />
+      </span>
+      <span className="social-icon">
+        <FaFacebookSquare />
+      </span>
+      <span className="social-icon">
+        <FaInstagramSquare />
+      </span>
+      <span className="social-icon">
+        <FaGithubSquare />
+      </span>
+    </footer>
+  )
 }
 
 export default Footer
